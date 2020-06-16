cfa60997c7c21a5d124488f1583fac9767ceef17
Killian Jacob <jacob.killian@outlook.fr>
2020-06-16 14:20:28 +0200

     

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
 
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/dealer.js"></script>
 <script src="js/bestCombo.js"></script>
@@ -12,15 +15,40 @@
 <script src="js/isAPair.js"></script>
 <script src="js/Utils.js"></script>
 <script src="js/compareCombos.js"></script>
+
 </head>
 
 <body>
+
+<div class="container-fluid p-0">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">This is not poker</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Jouez <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nos engagement</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Les partenaires</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</div>
 
 <div class="container">
 
     <div class="row mt-5">
 
-        <div class="col text-center">
+        <div class="col text-center Glower" id="hero">
 
             <div class="row text-center mb-3">
                 <div class="col">
@@ -44,11 +72,11 @@
 
         <div class="col text-center">
 
-            <button type="button" class="btn btn-secondary">Jouer !</button>
+            <button type="button" id="play" class="btn btn-secondary">Jouer !</button>
 
         </div>
 
-        <div class="col mb-3 text-center">
+        <div class="col mb-3 text-center Glower" id="vilain">
 
         <div class="row text-center mb-3">
                 <div class="col">
@@ -103,11 +131,137 @@
     </div>
 </div>
 
+<div class="container-fluid ">
+
+<div class="row text-center bg-light mt-5 h-25">
+
+    <div class="col-4 offset-2">
+
+        <img src="assets/responsive.png" class="img-fluid h-100 p-3" alt="">
+
+    </div>
+    <div class="col-4">
+
+        <div class="row">
+
+            <div class="col">
+
+                <h2>Le jeu qui vous ressemble</h2>
+
+            </div>
+        </div>
+
+        <div class="row mt-3">
+
+            <div class="col">
+            <i class="far fa-grin-beam fa-4x"></i>
+                <h5 class="mt-2">Fun</h5>
+            </div>
+            <div class="col">
+                <i class="fas fa-lock fa-4x"></i>
+                <h5 class="mt-2">Securisé</h5>
+            </div>
+            <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+                <h5 class="mt-2">Experience de jeu</h5>
+            </div>           
+        </div>
+
+    </div>
+</div>
+
+<div class="row text-center mt-4">
+
+    <div class="col">
+
+        <h3>Nos partenaires</h3>
+
+        <div class="row mt-5">
+
+            <div class="col">
+
+                <h5>Plop game</h5>
+                <img src="assets/kawaii-game.png" class="img-fluid" alt="">
+
+            </div>
+
+            <div class="col">
+
+                <h5>Club not poker</h5>
+                <img src="assets/chips-forum.png" class="img-fluid" alt="">
+
+            </div>
+            <div class="col">
+
+                <h5>Deuces Magazine</h5>
+                <img src="assets/aces-magazine.png" class="img-fluid" alt="">
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="row text-center bg-light pt-3 pb-5">
+
+    <div class="col">
+
+        <h5>TINP</h5>
+        <hr> 
+        <a>Nous contacter</a></br>
+        <a>Nous rejoindre</a></br>
+        <a>FAQ</a></br>
+    </div>
+    <div class="col">
+
+    <h5>Le jeu</h5>
+    <hr> 
+    <a>Les règles</a></br>
+    <a>Histoire</a></br>
+    </div>
+    <div class="col">
+
+    <h5>Les bonus</h5>
+    <hr> 
+    <a>Premier dépot</a></br>
+    <a>Early Adopter</a></br>
+    <a>Parrainage</a></br>
+    </div>
+    <div class="col">
+
+    <h5>Nos partenaires</h5>
+    <hr> 
+    <a>Plop game</a></br>
+    <a>Club Not Poker</a></br>
+    <a>Deuces Magazine</a></br>
+    </div>
+    <div class="col p-1">
+
+        <div class="row">
+
+        <img src="assets/stripe.png" class="mx-auto" height="50" alt="">
+
+        </div>
+        <div class="row">
+
+        <img src="assets/visa.png" class="mx-auto" height="50" alt="">
+
+        </div>
+    </div>
+</div>
 </body>
 
 <script>
 
-var hands = dealer();
+$(document).ready(function() {
+    $("#play").click(function(){
+        startGame();
+    }); 
+});
+
+function startGame(){
+
+    var hands = dealer();
 console.log(hands[0]);
 console.log(hands[1]);
 console.log(hands[2]);
@@ -146,11 +300,29 @@ console.log("Vilain : " + resVilain);
 if(arraysEqual(WinHand,comboHero)){
     
     console.log("Winner : hero");
+    var hero = $('#hero');
+    var vilain = $('#vilain');
+
+    vilain.removeClass("active");
+
+    window.setInterval(function() {  
+        hero.toggleClass('active');
+    }, 1000);
 
 }
 else if(arraysEqual(WinHand,comboVilain)){
 
     console.log("Winner : vilain");
+    var hero = $('#hero');
+    var vilain = $('#vilain');
+
+    hero.removeClass("active");
+
+    window.setInterval(function() {  
+        vilain.toggleClass('active');
+    }, 1000);
+}
+
 
 }
 
--- js/compareCombos.js
+++ js/compareCombos.js
@@ -7,11 +7,33 @@ function compareCombos(cards1, cards2) {
 
 function compare(cards1, cards2) {
 
-	if (getHandValue(cards1) > getHandValue(cards2)) return -1
-	else if (getHandValue(cards1) < getHandValue(cards2)) return 1
+	if (getHandValue(cards1) > getHandValue(cards2)){
+		
+		console.log("Hand 1 : " + getHandValue(cards1));
+		console.log("Hand 2 : " + getHandValue(cards2));
+		return -1
+
+	}
+	else if (getHandValue(cards1) < getHandValue(cards2)){
+		
+		console.log("Hand 1 : " + getHandValue(cards1));
+		console.log("Hand 2 : " + getHandValue(cards2));		
+		return 1
+	}
 	else {
-		if (getHandValue(cards1) > getHandValue(cards2)) return -1
-		else return 1
+		if (getHandValue(cards1) > getHandValue(cards2))
+		{ 
+			console.log("Hand 1 : " + getHandValue(cards1));
+			console.log("Hand 2 : " + getHandValue(cards2));					
+			return -1
+
+		}
+		else{
+
+			console.log("Hand 1 : " + getHandValue(cards1));
+			console.log("Hand 2 : " + getHandValue(cards2));		
+		 return 1
+		}
 	}
 }
 
@@ -62,7 +84,7 @@ function getHandValue(cards) {
 		}
 	}
 	if (isAThree && isATwo) {
-		return 4
+		return 4 * getTotalValue(cards) * (2000/100);
 	}
 
 	// Check for a flush
@@ -76,7 +98,7 @@ function getHandValue(cards) {
 				}
 			}
 		)
-		return 3
+		return 3 * getTotalValue(cards)* (1000/100);
 	}
 
 	// Check for a pair
@@ -90,11 +112,11 @@ function getHandValue(cards) {
 				}
 			}
 		)
-		return 2
+		return 2 * getTotalValue(cards)* (200/100);
 	}
 
 	// Check for a hauteur
-	return 1
+	return 1 * getTotalValue(cards) * (100/100);
 }
 
 function allIndexOf(arr, value) {
@@ -103,7 +125,7 @@ function allIndexOf(arr, value) {
 		for( let i = arr.indexOf(value); i >= 0; i = arr.indexOf(value,i+1) ) {
 			res.push(i);
 		}
-		return res
+		return res 
 	}
 	else return false
 }
@@ -112,4 +134,150 @@ function nbOccurences(arr, value) {
 	let indexes = allIndexOf(arr, value)
 	if (indexes != false) return indexes.length
 	else return false
+}
+
+function getTotalValue(cards) {
+
+	let total_value = 0;
+
+	cards = orderCards(cards);
+
+	var TypeValue = {
+		'2': '02',
+		'3': '03',
+		'4': '04',
+		'5': '05',
+		'6': '06',
+		'7': '07',
+		'8': '08',
+		'9': '09',
+		'10': '10',
+		'J': '11',
+		'Q': '12',
+		'K': '13',
+		'A': '14',		
+	};
+	
+	var symbole = {
+		'd' : 1,
+		'c' : 2,
+		'h' : 3,
+		's' : 4,	
+	}
+
+	for (let i = 0; i < cards.length; i++) {
+	
+		var c = cards[i];
+
+		var value = null;
+		var type = null;
+
+		if(c.length == 3){
+	
+			value = c.substring(0, 2);
+			type = c.charAt(2);
+		
+		}
+		else{
+	
+			value = c.charAt(0);
+			type = c.charAt(1);
+			
+		}
+	    total_value = total_value + (TypeValue[value]);
+		
+
+}
+return total_value;
+
+}
+
+function orderCards(cards) {
+
+	let arrValue = {};
+	
+	let TypeValue = {
+		'2': 2,
+		'3': 3,
+		'4': 4,
+		'5': 5,
+		'6': 6,
+		'7': 7,
+		'8': 8,
+		'9': 9,
+		'10': 10,
+		'J': 11,
+		'Q': 12,
+		'K': 13,
+		'A': 14,		
+	};
+	
+	let symbole = {
+		'd' : 4,
+		'c' : 3,
+		'h' : 2,
+		's' : 1,	
+	}
+  
+	var temp_arr = [];
+  
+	for (let i = 0; i < cards.length; i++) {
+	
+	
+		for (let a = i; a < cards.length; a++) {
+	
+			var c = cards[i];
+	
+			var value = null;
+		
+			var type = null;
+		
+			if(c.length == 3){
+		
+				value = c.substring(0, 1);
+				type = c.charAt(2);
+			
+		
+			}
+			else{
+		
+				value = c.charAt(0);
+				type = c.charAt(1);
+				
+			}
+
+			var c2 = cards[a];
+	
+			var value2 = null;
+			var type2 = null;
+	
+			if(c2.length == 3){
+		
+				value2 = c2.substring(0, 1);
+				type2 = c2.charAt(2);
+			}
+			else{
+		
+				value2 = c2.charAt(0);
+				type2 = c2.charAt(1)
+			}			
+	
+	
+			if(TypeValue[value2] > TypeValue[value] || ( TypeValue[value2] >= TypeValue[value] && symbole[type] >= symbole[type2] ) ){
+				
+				var tempValue = c;
+				
+				cards[i] = cards[a];
+				cards[a] = tempValue;
+				
+			}
+	
+		}
+	
+		
+	
+	}
+
+	return cards;
+
 }
